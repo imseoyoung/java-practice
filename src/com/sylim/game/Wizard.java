76003e92372be6240ab78be94beb8671712ae760
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-14 17:05:40 +0900

    Wizard, Wand 수정

--- src/com/sylim/game/Wizard.java
+++ src/com/sylim/game/Wizard.java
@@ -6,9 +6,33 @@ public class Wizard {
     private int mp;
     private Wand wand;
     
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getHP() {
+        return hp;
+    }
+    
+    public void setHP(int hp) {
+        this.hp = hp;
+    }
+    
+    public int getMP() {
+        return mp;
+    }
+    
+    public void setMP(int mp) {
+        this.mp = mp;
+    }
+    
     public void heal(Hero hero) {
         int basePoint = 10;
-        int recovPoint = (int) (basePoint * this.wand.power);
-        hero.setHp(hero.getHP() + recovPoint);
+        int recovPoint = (int) (basePoint * this.wand.getPower());
+        hero.setHP(hero.getHP() + recovPoint);
     }
 }
