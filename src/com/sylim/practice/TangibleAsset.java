db120c10c51617e563071775beab04d21c61c0d8
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-15 15:58:07 +0900

    TangibleAsset 수정

--- src/com/sylim/practice/TangibleAsset.java
+++ src/com/sylim/practice/TangibleAsset.java
@@ -1,7 +1,21 @@
 package com.sylim.practice;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
+    private double weight;
+    // Asset으로부터 상속
     public TangibleAsset(String name, int price, String color) {
         super(name, price, color);
+    }
+    
+    // Thing의 메소드
+    // 추상 클래스이기 때문에 필수적으로 오버라이딩 해야하는 것은 아니나,
+    // Book과 Computer에서 공통적으로 구현될 메소드이기 때문에 부모 클래스인 TangibleAsset에 하는 것이 적합하다고 봄
+    @Override
+    public double getWeight() {
+        return weight;
+    };
+    @Override
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 }
