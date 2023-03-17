21f7c2268c5c22790dee2f089c11f5eab4056347
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-17 16:13:07 +0900

    if문 -> switch 문

--- src/com/sylim/practice/examUp4/StrongBox.java
+++ src/com/sylim/practice/examUp4/StrongBox.java
@@ -1,6 +1,5 @@
 package com.sylim.practice.examUp4;
 
-// generic = type에 제한을 둔다
 public class StrongBox<T> {
     private T data;
     private int count;
@@ -13,21 +12,28 @@ public class StrongBox<T> {
 
     // 열쇠의 종류를 받는 생성자
     public StrongBox(KeyType key) {
-        if (key == padlock) {
-            count = 1024;
-        } else if (key == button) {
-            count = 10000;
-        } else if (key == dial) {
-            count = 30000;
-        } else {
-            count = 1000000;
+        switch(key) {
+            case PADLOCK:
+                count = 1024;
+                break;
+            case BUTTON:
+                count = 10000;
+                break;
+            case DIAL:
+                count = 30000;
+                break;
+            case FINGER:
+                count = 1000000;
+                break;
+            default: 
         }
     }
 
     public void put(T data) {
         this.data = data;
     }
-
+    
+    // 열쇠의 종류에 따라 미리 정해진 count를 호출될때마다 1씩 감소시킨다.
     public T get() {
         count--;
         if (count > 0) {
@@ -37,14 +43,12 @@ public class StrongBox<T> {
     }
 
     public static void main(String[] args) {
-        StrongBox<String> button = new StrongBox<>(KeyType.BUTTON);
-        button.put(new String("과자"));
+        StrongBox<String> box = new StrongBox<>(KeyType.BUTTON);
+        box.put(new String("과자"));
 
         for (int i = 0; i < 10000; i++) {
-             //button.get();
-             System.out.println(button.get());
+             System.out.println(box.get());
         }
-        //System.out.println();
 
     }
 }
