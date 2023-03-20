4d7972cf353717e5f68b1b41466e147508659c25
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-20 17:28:08 +0900

    연습문제 6-2

--- src/com/sylim/practice/examUp6/Main.java
+++ src/com/sylim/practice/examUp6/Main.java
@@ -1,8 +1,12 @@
 package com.sylim.practice.examUp6;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class Main {
     public static void main(String args[]) {
@@ -14,18 +18,55 @@ public class Main {
             // 버퍼 생성
             byte[] buffer = new byte[1024];
             // 버퍼의 크기는 필요에 따라 변경할 수 있다
+            
+            // 버퍼링
+            BufferedInputStream bins = new BufferedInputStream(fis);
+            BufferedOutputStream bons = new BufferedOutputStream(fos);
+            
 
             // 복사
-            int bytesRead = fis.read(buffer);
+            int bytesRead = bins.read(buffer);
             while (bytesRead != -1) {
-                fos.write(buffer, 0, bytesRead);
-                bytesRead = fis.read(buffer);
+                bons.write(buffer, 0, bytesRead);
+                bytesRead = bins.read(buffer);
             }
             
-           fis.close();
-           fos.close();
-           
-           System.out.println("복사 완료");
+            fis.close();
+            fos.close();
+            bins.close();
+
+            // 복사한 파일 압축
+            GZIPOutputStream gzipos = new GZIPOutputStream(new FileOutputStream("data_copy.zip"));
+            FileInputStream fis2 = new FileInputStream("data_copy.txt");
+            
+            BufferedInputStream bins2 = new BufferedInputStream(fis2);
+            
+            int len;
+            while ((len = bins2.read(buffer)) > 0) {
+                gzipos.write(buffer, 0, len);
+            }
+            
+            fis2.close();
+            bins2.close();
+            gzipos.finish();
+            gzipos.close();
+            System.out.println("압축 완료");
+            
+            // 압축 풀기
+            GZIPInputStream gzipis = new GZIPInputStream(new FileInputStream("data_copy.zip"));
+            FileOutputStream fos2 = new FileOutputStream("data_copy_zip.txt");
+            
+            BufferedOutputStream bons2 = new BufferedOutputStream(fos2);
+
+            // 압축 해제
+            while ((len = gzipis.read(buffer)) > 0) {
+                bons2.write(buffer, 0, len);
+            }
+            fos2.close();
+            bons2.close();
+            gzipis.close();
+            System.out.println("압축 해제 완료");
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
