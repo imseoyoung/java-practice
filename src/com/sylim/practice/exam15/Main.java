682c7b0d8b5f6e69e70b3fd3fa851626bb3291a2
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-20 17:07:51 +0900

    연습문제 15-3

--- src/com/sylim/practice/exam15/Main.java
+++ src/com/sylim/practice/exam15/Main.java
@@ -3,13 +3,12 @@ package com.sylim.practice.exam15;
 public class Main {
     public static void main(String[] args) {
         try {
-            String s = null;
-            int length = s.length();
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException 예외를 catch 하였습니다");
-            System.out.println("---- stack trace(여기부터) ----");
-            e.printStackTrace();
-            System.out.println("---- stack trace(여기까지) ----");
+            String str = "Three";
+                        // Integer.parseInt() 메소드는 문자열을 정수로 바꿔준다.
+            int i = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            System.out.println("10진수 정수를 나타내는 문자여야 합니다. " + e.getMessage());
+            e.printStackTrace();          
         }
     }
 }
