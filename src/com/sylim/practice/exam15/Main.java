ca8cba3f0cc6c3b3ade2f5aa9ac32fd8c1d052a9
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-20 17:07:13 +0900

    연습문제 15-2

--- src/com/sylim/practice/exam15/Main.java
+++ src/com/sylim/practice/exam15/Main.java
@@ -2,7 +2,14 @@ package com.sylim.practice.exam15;
 
 public class Main {
     public static void main(String[] args) {
-        String s = null;
-        int length = s.length();
+        try {
+            String s = null;
+            int length = s.length();
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException 예외를 catch 하였습니다");
+            System.out.println("---- stack trace(여기부터) ----");
+            e.printStackTrace();
+            System.out.println("---- stack trace(여기까지) ----");
+        }
     }
 }
