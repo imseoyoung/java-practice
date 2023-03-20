31b45a85bedd1e8fedf5ae177cffe8fcd1748b3b
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-20 17:08:22 +0900

    연습문제 15-4

--- src/com/sylim/practice/exam15/Main.java
+++ src/com/sylim/practice/exam15/Main.java
@@ -1,13 +1,15 @@
 package com.sylim.practice.exam15;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            String str = "Three";
-                        // Integer.parseInt() 메소드는 문자열을 정수로 바꿔준다.
-            int i = Integer.parseInt(str);
-        } catch (NumberFormatException e) {
-            System.out.println("10진수 정수를 나타내는 문자여야 합니다. " + e.getMessage());
+            System.out.println("프로그램 시작");
+            FileWriter fw = new FileWriter("data/data.txt"); // 잘못된 파일 경로
+        } catch (IOException e) {
+            System.out.println("에러 발생");
             e.printStackTrace();          
         }
     }
