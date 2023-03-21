f05da9dcdf9823012ceeb5ced3af835cf6f9d7cc
imseoyoung <lsy783@sookmyung.ac.kr>
2023-03-21 09:05:53 +0900

    패키지 이름 수정

---
+++ src/com/sylim/practice/examUp7/Main2.java
@@ -0,0 +1,17 @@
+package com.sylim.practice.examUp7;
+
+import java.io.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        Department department = new Department("총무부", new Employee("홍길동", 41));
+        
+        // 저장
+        FileOutputStream fos = new FileOutputStream("company.dat");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(department);
+        oos.flush();
+        oos.close();
+        
+    }
+}
